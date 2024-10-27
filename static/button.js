0a53da57c54394e610a22155d37cda17dd2e2f42
Paulo <paulo3252@myyahoo.com>
2024-10-26 17:50:56 -0700

    Oct 26 2024

---
+++ static/button.js
@@ -0,0 +1,12 @@
+window.addEventListener("scroll", () => {
+    const backToTopButton = document.getElementById("backToTop");
+    if (window.scrollY > 100) {
+        backToTopButton.classList.add("show");
+    } else {
+        backToTopButton.classList.remove("show");
+    }
+});
+
+function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+}
